fe64b05ec317cfb730eaac8caff2d877813a1132
NitzanWainshtein <Nitzanwa@gmail.com>
2025-04-19 19:57:53 +0300

    few additions

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,20 @@ using namespace theMatrix;
 
 int main() {
     try {
-        SquareMat mat(3);
-        mat.setFullMatrix();
+        SquareMat mat1(2);
+        mat1.setFullMatrix();
+        SquareMat mat2(2);
+        mat2.setFullMatrix();
 
         // נגיד שכבר מילאנו את הערכים של mat בדרך כלשהי
         // כעת מדפיסים בעזרת האופרטור שהגדרנו
-        cout << mat << endl;
+        cout << "mat1:" << endl;
+        cout << mat1 << endl;
+        cout << "mat2:" << endl;
+        cout << mat2 << endl;
+        cout << "mat multiplied:" << endl;
+        cout << mat1*mat2 << endl;
+
     }
     catch(const exception& e){
         cerr << "Error: " << e.what() << endl;
--- SquareMat.h
+++ SquareMat.h
@@ -5,7 +5,7 @@
 #ifndef SQUAREMAT_H
 #define SQUAREMAT_H
 #include <stdexcept>
-
+#include <iostream>
 namespace theMatrix {
     class SquareMat {
     private:
@@ -13,11 +13,17 @@ namespace theMatrix {
         double **matrix;
 
     public:
-        SquareMat(int n); //Constructor
+        explicit SquareMat(int n); //Constructor
+        SquareMat(const SquareMat& other); //Copy constructor
+        SquareMat& operator=(const SquareMat& other); //Assignment constructor
         ~SquareMat(); //Destructor
         void setSingleValue(int row , int col , double value);
         void setFullMatrix();
         friend std::ostream& operator<<(std::ostream& os, const SquareMat& mat);
+        friend SquareMat operator+(const SquareMat& a, const SquareMat& b);
+        friend SquareMat operator-(const SquareMat& a, const SquareMat& b);
+        SquareMat operator-() const;
+        friend SquareMat operator*(const SquareMat& a, const SquareMat& b);
 
     };
 } // theMatrix
